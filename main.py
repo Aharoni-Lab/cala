0c656f9ec2dcc679c3ab55ab49de2e2b8e088d85
Raymond W. Chang <raymond.w.jang@gmail.com>
2024-11-18 18:19:05 -0800

    test: modify main to demonstrate pipeline manipulation

--- main.py
+++ main.py
@@ -9,8 +9,6 @@ def main():
     io = DataIO(video_paths=["video_1", "video_2"])
     io.save_raw_video("path_to_interim", "data_name")
 
-    data = io.load_data(stage="init")
-
     core_axes = ["height", "width"]
     iter_axis = "frames"
     video_dimensions = tuple(core_axes + [iter_axis])
@@ -19,12 +17,23 @@ def main():
     denoiser = Denoiser(method="median", core_axes=core_axes, kwargs={"ksize": 7})
     glow_remover = GlowRemover(iter_axis=iter_axis)
     background_eraser = BackgroundEraser(core_axes=core_axes)
-
     preprocessor = make_pipeline(downsampler, denoiser, glow_remover, background_eraser)
-
-    preprocessed_data = preprocessor.fit_transform(data)
 
     rigid_translator = RigidTranslator(core_axes=core_axes, iter_axis=iter_axis)
     motion_corrector = make_pipeline(rigid_translator)
 
+    data = io.load_data(stage="init")
+
+    # Option 1:
+    preprocessed_data = preprocessor.fit_transform(data)
     motion_corrected_data = motion_corrector.fit_transform(preprocessed_data)
+    demixed_data = demixer.fit_transform(motion_corrected_data)
+    deconvolved_data = deconvolver.fit_transform(demixed_data)
+
+    # Option 2:
+    cala_pipeline = make_pipeline(preprocessor, motion_corrector, demixer, deconvolver)
+    deconvolved_data = cala_pipeline.fit_transform(data)
+
+
+if __name__ == "__main__":
+    main()
