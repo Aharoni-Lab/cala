0b5489481f4031401976564a82b0fe6b4c46adf0
Raymond W. Chang <raymond.w.jang@gmail.com>
2024-11-21 18:58:21 -0800

    refactor: move filtration.py to a filtration package

---
+++ src/cala/cell_recruitment/filtration/base.py
@@ -0,0 +1,27 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass, field
+from typing import List
+
+from sklearn.base import BaseEstimator, TransformerMixin
+
+
+@dataclass
+class BaseFilter(BaseEstimator, TransformerMixin, ABC):
+    core_axes: List[str] = field(default_factory=lambda: ["width", "height"])
+    iter_axis: str = "frames"
+
+    @abstractmethodS
+    def fit_kernel(self, X):
+        pass
+
+    @abstractmethod
+    def transform_kernel(self, X, y):
+        pass
+
+    @abstractmethod
+    def fit(self, X, y=None):
+        pass
+
+    @abstractmethod
+    def transform(self, X, y=None):
+        pass
