e48844e6c497fbd8e676e4c6bce3f43554338afb
Raymond W. Chang <raymond.w.jang@gmail.com>
2024-12-14 13:28:36 +0900

    tests: debug the phenomenon where random number is fixed. (all cell spikes were identical)

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,8 +7,9 @@ from .fixtures import params, raw_calcium_video, preprocessed_video, stabilized_
 
 @pytest.fixture(autouse=True)
 def mock_random(monkeypatch: pytest.MonkeyPatch):
+    rs = RandomState(12345)
+
     def stable_random(*args, **kwargs):
-        rs = RandomState(12345)
         return rs.random(*args, **kwargs)
 
     monkeypatch.setattr("numpy.random.random", stable_random)
