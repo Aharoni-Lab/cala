8186fdda79154c0bf33af09ea9878e8004c6c78d
Raymond W. Chang <raymond.w.jang@gmail.com>
2025-02-06 16:06:40 -0800

    feat: implement streaming downsample, in spatial axes. test: implement tests for streaming downsample

--- src/cala/streaming_service/preprocess/__init__.py
+++ src/cala/streaming_service/preprocess/__init__.py
@@ -1,4 +1,12 @@
 from .background_removal import BackgroundEraser, BackgroundEraserParams
 from .denoise import Denoiser, DenoiserParams
+from .downsample import Downsampler, DownsamplerParams
 
-__all__ = ["BackgroundEraser", "BackgroundEraserParams", "Denoiser", "DenoiserParams"]
+__all__ = [
+    "BackgroundEraser",
+    "BackgroundEraserParams",
+    "Denoiser",
+    "DenoiserParams",
+    "Downsampler",
+    "DownsamplerParams",
+]
