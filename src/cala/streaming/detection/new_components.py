e61d7ebf447db72cf0f8601e612a79c3044ca774
Raymond W. Chang <raymond.w.jang@gmail.com>
2025-02-28 01:34:09 -0800

    chore: remove estimates.py since it's replaced by components

--- src/cala/streaming/detection/new_components.py
+++ src/cala/streaming/detection/new_components.py
@@ -1,8 +1,6 @@
 import numpy as np
 from river.base import SupervisedTransformer
 
-from cala.streaming.initialization.estimates import Estimates
-
 
 class NewComponentDetector(SupervisedTransformer):
     """Detects new components in residuals"""
@@ -11,21 +9,12 @@ class NewComponentDetector(SupervisedTransformer):
         pass
 
     def _add_new_components(
-        self, new_components: dict, estimates: Estimates
-    ) -> Estimates:
+            self,
+            new_components: dict,
+    ) -> np.ndarray:
         """Add newly detected components to estimates
 
         Args:
             new_components: Dictionary with new spatial and temporal components
         """
-        if estimates.spatial_footprints is None:
-            estimates.spatial_components = new_components["spatial"]
-            estimates.temporal_components = new_components["temporal"]
-        else:
-            estimates.spatial_footprints = np.hstack(
-                [estimates.spatial_footprints, new_components["spatial"]]
-            )
-            estimates.temporal_traces = np.vstack(
-                [estimates.temporal_traces, new_components["temporal"]]
-            )
-        return estimates
+        return np.array([])
