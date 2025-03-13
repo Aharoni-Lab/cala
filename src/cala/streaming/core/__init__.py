e2c073b35c31c5685dda019acf529c6f05d46136
Raymond W. Chang <raymond.w.jang@gmail.com>
2025-03-13 03:49:50 -0700

    debug: change import order to fix circular import

--- src/cala/streaming/core/__init__.py
+++ src/cala/streaming/core/__init__.py
@@ -1,6 +1,6 @@
-from .exchange import DataExchange
-from .parameters import Parameters
 from .store import BaseStore
+from .parameters import Parameters
 from .transformer_meta import TransformerMeta
+from .exchange import DataExchange
 
 __all__ = ["BaseStore", "Parameters", "TransformerMeta", "DataExchange"]
