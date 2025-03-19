d398ed55e43b725cc1374403423ad1f6f1a74b3e
Raymond W. Chang <raymond.w.jang@gmail.com>
2025-03-19 13:37:03 -0700

    debug: separate component out of types
    Signed-off-by: Raymond W. Chang <raymond.w.jang@gmail.com>

--- src/cala/streaming/core/__init__.py
+++ src/cala/streaming/core/__init__.py
@@ -1,13 +1,22 @@
+from .components import Component, ComponentTypes
 from .parameters import Parameters
-from .stores import Observable, Footprints, Traces, Component, ComponentTypes
+from .stores import (
+    ObservableStore,
+    FootprintStore,
+    TraceStore,
+    Footprints,
+    Traces,
+)
 from .transformer_meta import TransformerMeta
 
 __all__ = [
     "Parameters",
-    "Observable",
+    "ObservableStore",
     "TransformerMeta",
     "Component",
     "ComponentTypes",
+    "FootprintStore",
     "Footprints",
+    "TraceStore",
     "Traces",
 ]
