474ef6a6be1b381586c31da53717fe27f2717119
Raymond W. Chang <raymond.w.jang@gmail.com>
2025-04-08 15:40:45 -0700

    tests: test package_frame
    Signed-off-by: Raymond W. Chang <raymond@physics.ucla.edu>
    Signed-off-by: Raymond W. Chang <raymond.w.jang@gmail.com>

---
+++ tests/test_streaming_service/unit/test_util/test_frame.py
@@ -0,0 +1,67 @@
+from datetime import datetime
+
+import numpy as np
+import pandas as pd
+import xarray as xr
+
+from cala.streaming.core.axis import Axis
+from cala.streaming.util.frame import package_frame
+
+
+def test_package_frame():
+    # Create a sample 2D numpy array
+    frame = np.random.randint(0, 256, size=(100, 200), dtype=np.uint16)
+    index = 5
+    timestamp = datetime(2023, 4, 8, 12, 0, 0)
+
+    # Transform the frame
+    dataarray = package_frame(frame, index, timestamp)
+
+    # Check the type
+    assert isinstance(dataarray, xr.DataArray)
+
+    # Check the dimensions
+    assert dataarray.dims == Axis.spatial_axes
+
+    # Check the coordinates
+    assert "index" in dataarray.coords
+    assert "timestamp" in dataarray.coords
+    assert dataarray.coords[Axis.spatial_axes[0]].size == frame.shape[0]
+    assert dataarray.coords[Axis.spatial_axes[1]].size == frame.shape[1]
+    assert dataarray.coords["index"].item() == index
+    assert pd.Timestamp(dataarray.coords["timestamp"].values) == pd.Timestamp(timestamp)
+
+    # Check the data
+    np.testing.assert_array_equal(dataarray.values, frame)
+
+    # Check the name
+    assert dataarray.name == "frame"
+
+
+def test_package_frame_datetimeless():
+    # Create a sample 2D numpy array
+    frame = np.random.randint(0, 256, size=(100, 200), dtype=np.uint16)
+    index = 5
+
+    # Transform the frame
+    dataarray = package_frame(frame, index)
+
+    # Check the type
+    assert isinstance(dataarray, xr.DataArray)
+
+    # Check the dimensions
+    assert dataarray.dims == Axis.spatial_axes
+
+    # Check the coordinates
+    assert "index" in dataarray.coords
+    assert "timestamp" in dataarray.coords
+    assert dataarray.coords[Axis.spatial_axes[0]].size == frame.shape[0]
+    assert dataarray.coords[Axis.spatial_axes[1]].size == frame.shape[1]
+    assert dataarray.coords["index"].item() == index
+    assert not dataarray.coords["timestamp"].item()
+
+    # Check the data
+    np.testing.assert_array_equal(dataarray.values, frame)
+
+    # Check the name
+    assert dataarray.name == "frame"
