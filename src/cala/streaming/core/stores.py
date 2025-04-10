2e2476876e29f4c6b9645c7cf13b0032d6075bc2
Raymond W. Chang <raymond.w.jang@gmail.com>
2025-04-09 20:23:39 -0700

    feat: stores all have common update interface feat: persistent storage option for observablestores.
    persistent storage might eventually need to get subclassed as a mixin. however, i wish to check "store.persistent" in distributor, which would be harder with a mixin.
    
    Signed-off-by: Raymond W. Chang <raymond@physics.ucla.edu>
    Signed-off-by: Raymond W. Chang <raymond.w.jang@gmail.com>

--- src/cala/streaming/core/stores.py
+++ src/cala/streaming/core/stores.py
@@ -1,13 +1,20 @@
-from dataclasses import dataclass
+from abc import ABC, abstractmethod
+from dataclasses import dataclass, field
+from pathlib import Path
+from typing import ClassVar
 
 import xarray as xr
 
 
 @dataclass
-class ObservableStore:
-    """Base class for observable objects in calcium imaging data."""
+class ObservableStore(ABC):
+    """Base class for observable object storage."""
 
-    _warehouse: xr.DataArray
+    _warehouse: xr.DataArray = field(init=False)
+
+    persistent: ClassVar[bool] = False
+    store_dir: str | Path | None = None
+    peek_size: int | None = None
 
     @property
     def warehouse(self) -> xr.DataArray:
@@ -16,3 +23,11 @@ class ObservableStore:
     @warehouse.setter
     def warehouse(self, value: xr.DataArray) -> None:
         self._warehouse = value
+
+    @abstractmethod
+    def update(self, data: xr.DataArray) -> None:
+        pass
+
+    @property
+    def store_path(self) -> Path:
+        return self.store_dir / self.__class__.__name__.lower()
