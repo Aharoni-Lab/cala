6f72ab15c725d28a5860523c10910ec5db684518
Raymond W. Chang <raymond.w.jang@gmail.com>
2025-05-05 14:23:18 -0700

    feat: runner needs stream_dir and socket_manager
    Signed-off-by: Raymond W. Chang <raymond.w.jang@gmail.com>

--- src/cala/streaming/composer/runner.py
+++ src/cala/streaming/composer/runner.py
@@ -13,6 +13,7 @@ import xarray as xr
 from river import compose
 
 from cala.config.pipe import Step, StreamingConfig
+from cala.connection_manager import ConnectionManager
 from cala.streaming.core import Parameters
 from cala.streaming.core.distribution import Distributor
 from cala.streaming.nodes import Node
@@ -32,6 +33,11 @@ class Runner:
     config: StreamingConfig
     """Configuration defining the pipeline structure and parameters."""
     output_dir: Path
+    """Directory where pipeline outputs will be stored."""
+    socket_manager: ConnectionManager | None = None
+    """Connection manager for streaming connections."""
+    stream_dir: Path | None = None
+    """Directory where streaming data chunks will be stored."""
 
     _buffer: Buffer = field(init=False)
     """Internal frame buffer for multi-frame operations."""
