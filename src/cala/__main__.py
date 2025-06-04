8b81b82c908aa7ccbb0294361f9342c81bebbd56
Raymond W. Chang <raymond.w.jang@gmail.com>
2025-06-04 10:46:48 -0700

    feat: remove testing codes from prod
    Signed-off-by: Raymond W. Chang <raymond.w.jang@gmail.com>

--- src/cala/__main__.py
+++ src/cala/__main__.py
@@ -12,9 +12,7 @@ app = typer.Typer()
 
 @app.command()
 def main(
-    config_path: Annotated[
-        Path, typer.Option(help="Path to your Cala configuration yaml file.")
-    ] = "cala_config.yaml",
+    config_path: Annotated[Path, typer.Option(help="Path to your Cala configuration yaml file.")],
 ) -> None:
     os.environ["CALA_CONFIG_PATH"] = str(config_path)
     uvicorn.run("cala.gui.__main__:app", host="127.0.0.1", port=8000, reload=True)
