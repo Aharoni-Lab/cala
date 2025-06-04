dc3a15d54f675189d5d966d10291e9313e5ef3b3
Raymond W. Chang <raymond.w.jang@gmail.com>
2025-06-04 10:49:24 -0700

    feat: eventually we want to be able to use % values for element sizes
    Signed-off-by: Raymond W. Chang <raymond.w.jang@gmail.com>

--- src/cala/config/gui.py
+++ src/cala/config/gui.py
@@ -2,14 +2,14 @@ from pydantic import BaseModel
 
 
 class MovieConfig(BaseModel):
-    width: int
-    height: int
+    width: int | str
+    height: int | str
     stream_url: str
 
 
 class PlotConfig(BaseModel):
-    width: int
-    height: int
+    width: int | str
+    height: int | str
     max_points: int
 
 
