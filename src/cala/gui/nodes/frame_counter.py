da980b3b90ab0ed9bd545e9b85af2f2f68a920a8
Raymond W. Chang <raymond.w.jang@gmail.com>
2025-06-05 09:17:51 -0700

    feat: subclass out send_through (needs to be refactored)
    Signed-off-by: Raymond W. Chang <raymond.w.jang@gmail.com>

--- src/cala/gui/nodes/frame_counter.py
+++ src/cala/gui/nodes/frame_counter.py
@@ -1,12 +1,9 @@
-import asyncio
 from dataclasses import dataclass
 
 import xarray as xr
-from fastapi import WebSocketDisconnect
 from river.base import Transformer
 
-from cala.gui import WebsocketMessage
-from cala.gui.dependencies import get_socket_manager
+from cala.gui.nodes.util import send_through
 from cala.streaming.core import Axis, Parameters
 
 
@@ -34,29 +31,3 @@ class FrameCounter(Transformer):
             "index": self.frame_count_,
         }
         send_through(payload)
-
-
-def send_through(payload: dict) -> None:
-    manager = get_socket_manager()
-
-    try:
-        loop = asyncio.get_running_loop()
-    except RuntimeError as e:
-        print(e)
-        loop = None
-
-    try:
-        for websocket in manager.active_connections:
-            message = WebsocketMessage(payload=payload)
-
-            if loop:
-                loop.create_task(manager.send_json(message.model_dump(mode="json"), websocket))
-            else:
-                asyncio.run(manager.send_json(message.model_dump(mode="json"), websocket))
-
-    # https://fastapi.tiangolo.com/reference/websockets/#fastapi.WebSocket.send
-    except WebSocketDisconnect:
-        print(f"Num Connections: {len(manager.active_connections)}")
-        if websocket in manager.active_connections:
-            manager.disconnect(websocket)
-        pass
---
+++ src/cala/gui/nodes/util.py
@@ -0,0 +1,32 @@
+import asyncio
+
+from fastapi import WebSocketDisconnect
+
+from cala.gui import WebsocketMessage
+from cala.gui.dependencies import get_socket_manager
+
+
+def send_through(payload: dict) -> None:
+    manager = get_socket_manager()
+
+    try:
+        loop = asyncio.get_running_loop()
+    except RuntimeError as e:
+        print(e)
+        loop = None
+
+    try:
+        for websocket in manager.active_connections:
+            message = WebsocketMessage(payload=payload)
+
+            if loop:
+                loop.create_task(manager.send_json(message.model_dump(mode="json"), websocket))
+            else:
+                asyncio.run(manager.send_json(message.model_dump(mode="json"), websocket))
+
+    # https://fastapi.tiangolo.com/reference/websockets/#fastapi.WebSocket.send
+    except WebSocketDisconnect:
+        print(f"Num Connections: {len(manager.active_connections)}")
+        if websocket in manager.active_connections:
+            manager.disconnect(websocket)
+        pass
