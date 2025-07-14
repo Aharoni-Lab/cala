539c12bc0b91f9975c23a271ac228fa74066ba81
Raymond <raymond.w.jang@gmail.com>
2025-07-14 10:25:00 -0700

    refactor: modularize cold start into energy, slice_nmf, catalog (+ register, updates)

---
+++ src/cala/streaming/nodes/init/cold/__init__.py
@@ -0,0 +1,6 @@
+from .energy import Energy, EnergyParams
+from .slice_nmf import SliceNMF, SliceNMFParams
+from .catalog import Cataloger, CatalogerParams
+
+
+__all__ = ["Energy", "EnergyParams", "SliceNMF", "SliceNMFParams", "Cataloger", "CatalogerParams"]
