290c488199d728c6aaa19864a79f50915bd94adf
Raymond <raymond.w.jang@gmail.com>
2025-07-23 22:02:09 -0700

    tests: prep pipe test

---
+++ tests/test_preprocess/test_prep.py
@@ -0,0 +1,14 @@
+from cala.core.execute import Executor
+from cala.models.spec import Pipe
+
+
+def test_prep():
+    tube = Pipe.from_specification("cala-prep")
+    runner = Executor(tube)
+    while True:
+        try:
+            runner.preprocess()
+        except RuntimeError as e:
+            assert isinstance(e, RuntimeError)
+            assert e.args == ("Generator node stopped its iteration",)
+            break
