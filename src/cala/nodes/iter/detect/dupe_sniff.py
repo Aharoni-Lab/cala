ffe23e9f0c133249ab26ebac4798845fc510cc98
Raymond <raymond.w.jang@gmail.com>
2025-07-26 04:45:47 -0700

    feat: cold_start nodes fit to noob

--- src/cala/nodes/iter/detect/dupe_sniff.py
+++ src/cala/nodes/iter/detect/dupe_sniff.py
@@ -4,7 +4,7 @@ import numpy as np
 import xarray as xr
 from noob.node import Node
 
-from cala.stores.common import Footprints, Traces
+from cala.models import AXIS, Footprint, Footprints, Residual, Trace, Traces
 
 
 class DupeSniffer(Node):
@@ -12,33 +12,26 @@ class DupeSniffer(Node):
 
     def process(
         self,
-        new_fp: xr.DataArray,
-        new_tr: xr.DataArray,
-        existing_fp: xr.DataArray,
-        existing_tr: xr.DataArray,
-        residuals: xr.DataArray,
+        new_fp: Footprint,
+        new_tr: Trace,
+        existing_fp: Footprints,
+        existing_tr: Traces,
+        residuals: Residual,
     ) -> list[tuple[str, float]] | None:
         """
         determines whether the new component overlaps with an existing component.
         if novel, return None.
         if similar to existing components (above threshold), return the component IDs.
-
-        :param new_fp:
-        :param new_tr:
-        :param existing_fp:
-        :param existing_tr:
-        :param residuals:
-        :return:
         """
 
-        overlapping_components = self._find_overlap_ids(new_fp, existing_fp)
+        overlapping_components = self._find_overlap_ids(new_fp.array, existing_fp.array)
 
         if not overlapping_components:
             return None
 
-        overlapped_traces = self._get_overlapped_traces(overlapping_components, existing_tr)
+        overlapped_traces = self._get_overlapped_traces(overlapping_components, existing_tr.array)
 
-        synced_traces = self._get_synced_traces(new_tr, overlapped_traces)
+        synced_traces = self._get_synced_traces(new_tr.array, overlapped_traces)
 
         if synced_traces:
             return synced_traces
@@ -46,7 +39,7 @@ class DupeSniffer(Node):
         return None
 
     def _find_overlap_ids(
-        self, new_footprints: Footprints, existing_footprints: Footprints
+        self, new_footprints: xr.DataArray, existing_footprints: xr.DataArray
     ) -> np.ndarray:
         if existing_footprints.size == 0:
             return np.array([])
@@ -54,36 +47,30 @@ class DupeSniffer(Node):
         overlaps = (new_footprints @ existing_footprints) > 0
         overlaps: xr.DataArray[bool]
 
-        return overlaps.where(overlaps, drop=True).coords[self.params.id_coord].values
+        return overlaps.where(overlaps, drop=True).coords[AXIS.id_coord].values
 
     def _get_overlapped_traces(
         self, overlapping_components: np.ndarray, existing_tr: xr.DataArray
     ) -> xr.DataArray:
         return (
-            existing_tr.set_xindex(self.params.id_coord)
-            .sel({self.params.id_coord: overlapping_components})
-            .reset_index(self.params.id_coord)
+            existing_tr.set_xindex(AXIS.id_coord)
+            .sel({AXIS.id_coord: overlapping_components})
+            .reset_index(AXIS.id_coord)
         )
 
     def _get_synced_traces(
-        self,
-        new_trace: Traces,
-        existing_traces: Traces,
+        self, new_trace: xr.DataArray, existing_traces: xr.DataArray
     ) -> list[tuple[str, float]]:
         """Validate new component against spatial and temporal criteria."""
 
         relevant_traces = existing_traces.isel(
-            {self.params.frames_dim: slice(-new_trace.sizes[self.params.frames_dim], None)}
+            {AXIS.frames_dim: slice(-new_trace.sizes[AXIS.frames_dim], None)}
         )
         # For components with spatial overlap, check temporal correlation
-        temporal_corr = xr.corr(
-            new_trace,
-            relevant_traces,
-            dim=self.params.frames_dim,
-        )
+        temporal_corr = xr.corr(new_trace, relevant_traces, dim=AXIS.frames_dim)
 
-        dupes = temporal_corr.where(temporal_corr >= self.params.merge_threshold, drop=True)
-        dupe_ids = dupes.coords[self.params.id_coord].values.tolist()
+        dupes = temporal_corr.where(temporal_corr >= self.merge_threshold, drop=True)
+        dupe_ids = dupes.coords[AXIS.id_coord].values.tolist()
         dupe_scores = dupes.values.tolist()
 
         return sorted(list(zip(dupe_ids, dupe_scores)), key=itemgetter(1))
--- tests/test_iter/test_cold_start.py
+++ tests/test_iter/test_cold_start.py
@@ -3,22 +3,20 @@ import numpy as np
 import pytest
 from noob.node import NodeSpecification
 
+from cala.models import AXIS, Residual
 from cala.models.observable import Footprint, Footprints, Trace, Traces
-from cala.nodes.iter.detect.catalog import Cataloger
-from cala.nodes.iter.detect.dupe_sniff import DupeSniffer
-from cala.nodes.iter.detect.energy import Energy
-from cala.nodes.iter.detect.slice_nmf import SliceNMF
+from cala.nodes.iter.detect import Cataloger, DupeSniffer, Energy, SliceNMF
 from cala.testing.toy import FrameDims, Position, Toy
 from cala.testing.util import assert_scalar_multiple_arrays
 
 
 @pytest.fixture(autouse=True, scope="module")
-def simulator():
-    n_frames = 300
+def toy():
+    n_frames = 30
     frame_dims = FrameDims(width=512, height=512)
     cell_positions = [Position(width=256, height=256)]
     cell_radii = 30
-    cell_traces = [np.array(range(300), dtype=float)]
+    cell_traces = [np.array(range(n_frames), dtype=float)]
     confidences = [0.8]
 
     return Toy(
@@ -31,198 +29,205 @@ def simulator():
     )
 
 
-@pytest.fixture(autouse=True, scope="module")
-def single_cell_video(simulator):
-    return simulator.make_movie()
+@pytest.fixture(autouse=True, scope="class")
+def single_cell_video(toy):
+    return toy.make_movie()
+
+
+@pytest.fixture(scope="class")
+def energy():
+    return Energy.from_specification(
+        spec=NodeSpecification(
+            id="test-energy",
+            type="cala.nodes.iter.detect.Energy",
+            params={"gaussian_std": 1.0},
+        )
+    )
+
+
+@pytest.fixture(scope="class")
+def energy_shape(energy, single_cell_video):
+    return energy.process(Residual(array=single_cell_video.array))
+
+
+@pytest.fixture(scope="class")
+def slice_nmf(toy):
+    return SliceNMF.from_specification(
+        spec=NodeSpecification(
+            id="test-slice-nmf",
+            type="cala.nodes.iter.detect.SliceNMF",
+            params={"cell_radius": 2 * toy.cell_radii[0], "validity_threshold": 0.8},
+        )
+    )
+
+
+@pytest.fixture(scope="class")
+def sniffer():
+    return DupeSniffer.from_specification(
+        spec=NodeSpecification(
+            id="test-dupe-sniffer",
+            type="cala.nodes.iter.detect.DupeSniffer",
+            params={"merge_threshold": 0.8},
+        )
+    )
+
+
+@pytest.fixture(scope="class")
+def cataloger():
+    return Cataloger.from_specification(
+        spec=NodeSpecification(id="test", type="cala.nodes.iter.detect.Cataloger")
+    )
 
 
 class TestEnergy:
-    @pytest.fixture(scope="class")
-    def energy(self):
-        return Energy.from_specification(
-            spec=NodeSpecification(
-                id="test", type="cala.nodes.iter.detect.energy.Energy", params={"gaussian_std": 1.0}
-            )
-        )
-
     def test_estimate_gaussian_noise(self, energy, single_cell_video):
-        noise_level = energy._estimate_gaussian_noise(single_cell_video, single_cell_video.shape)
+        noise_level = energy._estimate_gaussian_noise(
+            single_cell_video.array, single_cell_video.array.shape
+        )
         print(f"\nNoise Level: {noise_level}")
 
     def test_center_to_median(self, energy, single_cell_video):
-        centered_video = energy._center_to_median(single_cell_video)
-        assert centered_video.max() < single_cell_video.max()
+        centered_video = energy._center_to_median(single_cell_video.array)
+        assert centered_video.max() < single_cell_video.array.max()
 
     def test_process(self, energy, single_cell_video):
-        energy_landscape = energy.process(single_cell_video)
-        plt.imsave("mean_video.png", single_cell_video.mean(dim=energy.params.frames_dim))
+        energy_landscape = energy.process(Residual(array=single_cell_video.array))
+        plt.imsave("mean_video.png", single_cell_video.array.mean(dim=AXIS.frames_dim))
         plt.imsave("energy.png", energy_landscape)
-        assert energy_landscape.sizes == single_cell_video[0].sizes
+        assert energy_landscape.sizes == single_cell_video.array[0].sizes
         assert np.all(energy_landscape >= 0)
 
 
 class TestSliceNMF:
-    @pytest.fixture(scope="module")
-    def energy_shape(self, single_cell_video):
-        return Energy.from_specification(
-            spec=NodeSpecification(
-                id="test", type="cala.nodes.iter.detect.energy.Energy", params={"gaussian_std": 1.0}
-            )
-        ).process(single_cell_video)
-
-    @pytest.fixture(scope="class")
-    def slice_nmf(self, simulator):
-        return SliceNMF.from_specification(
-            spec=NodeSpecification(
-                id="test",
-                type="cala.nodes.iter.detect.slice_nmf.SliceNMF",
-                params={"cell_radius": 2 * simulator.cell_radii[0], "validity_threshold": 0.8},
-            )
-        )
-
     def test_get_max_energy_slice(self, slice_nmf, single_cell_video, energy_shape):
-        slice_ = slice_nmf._get_max_energy_slice(single_cell_video, energy_shape)
+        slice_ = slice_nmf._get_max_energy_slice(single_cell_video.array, energy_shape)
         return slice_
 
-    def test_local_nmf(self, slice_nmf, single_cell_video, energy_shape, simulator):
-        slice_ = slice_nmf._get_max_energy_slice(single_cell_video, energy_shape)
+    def test_local_nmf(self, slice_nmf, single_cell_video, energy_shape, toy):
+        slice_ = slice_nmf._get_max_energy_slice(single_cell_video.array, energy_shape)
         footprint, trace = slice_nmf._local_nmf(
             slice_,
-            simulator.frame_dims.model_dump(),
+            toy.frame_dims.model_dump(),
         )
 
-        # Using the Pythagorean Theorem
-        abab = (footprint @ simulator.footprints) ** 2
-        aabb = footprint.dot(footprint) * simulator.footprints.dot(simulator.footprints)
+        assert_scalar_multiple_arrays(footprint, toy.footprints.array)
 
-        assert abab > aabb * 0.99999997
-
-    def test_check_validity(self, slice_nmf, single_cell_video, energy_shape, simulator):
+    def test_check_validity(self, slice_nmf, single_cell_video, energy_shape, toy):
         """
         test this with multiple overlapping cells!!
         """
-        slice_ = slice_nmf._get_max_energy_slice(single_cell_video, energy_shape)
+        slice_ = slice_nmf._get_max_energy_slice(single_cell_video.array, energy_shape)
         footprint, _ = slice_nmf._local_nmf(
             slice_,
-            simulator.frame_dims.model_dump(),
+            toy.frame_dims.model_dump(),
         )
-        assert slice_nmf._check_validity(footprint, single_cell_video)
+        assert slice_nmf._check_validity(footprint, single_cell_video.array)
 
-    def test_process(self, slice_nmf, single_cell_video, energy_shape, simulator):
-        new_component = slice_nmf.process(single_cell_video, energy_shape)
+    def test_process(self, slice_nmf, single_cell_video, energy_shape, toy):
+        new_component = slice_nmf.process(Residual(array=single_cell_video.array), energy_shape)
         if new_component:
             new_fp, new_tr = new_component
         else:
             raise AssertionError("Failed to detect a new component")
 
-        for new, old in zip([new_fp, new_tr], [simulator.footprints, simulator.traces]):
-            assert_scalar_multiple_arrays(new, old)
+        for new, old in zip([new_fp, new_tr], [toy.footprints, toy.traces]):
+            assert_scalar_multiple_arrays(new, old.array)
 
 
 class TestSniffer:
-    @pytest.fixture(scope="module")
-    def energy_shape(self, single_cell_video):
-        return Energy(EnergyParams(gaussian_std=1.0)).process(single_cell_video)
-
     @pytest.fixture(scope="class")
-    def new_component(self, single_cell_video, energy_shape):
-        return SliceNMF(SliceNMFParams(cell_radius=10, validity_threshold=0.8)).process(
-            single_cell_video, energy_shape
-        )
+    def new_component(self, slice_nmf, single_cell_video, energy_shape):
+        return slice_nmf.process(Residual(array=single_cell_video.array), energy_shape)
 
-    @pytest.fixture(scope="class")
-    def sniffer(self):
-        return DupeSniffer(params=DupeSnifferParams(merge_threshold=0.8))
-
-    def test_find_overlap_ids(self, simulator, new_component, sniffer):
+    def test_find_overlap_ids(self, toy, new_component, sniffer):
         new_fp, new_tr = new_component
 
-        simulator.add_cell(
-            Position(width=260, height=260),
-            simulator.cell_radii[0],
-            simulator.traces[0][::-1],
-            "cell_1",
+        toy.add_cell(
+            position=Position(width=260, height=260),
+            radius=toy.cell_radii[0],
+            trace=toy.traces.array[0][::-1].values,
+            id_="cell_1",
         )
 
-        ids = sniffer._find_overlap_ids(new_fp, simulator.footprints)
+        ids = sniffer._find_overlap_ids(new_fp, toy.footprints.array)
         assert np.all(ids == ["cell_0", "cell_1"])
 
-        simulator.drop_cell("cell_1")
+        toy.drop_cell("cell_1")
 
-    def test_get_overlapped_traces(self, simulator, new_component, sniffer):
+    def test_get_overlapped_traces(self, toy, new_component, sniffer):
         new_fp, _ = new_component
-        ids = sniffer._find_overlap_ids(new_fp, simulator.footprints)
+        ids = sniffer._find_overlap_ids(new_fp, toy.footprints.array)
 
-        trace = sniffer._get_overlapped_traces(ids, simulator.traces)
+        trace = sniffer._get_overlapped_traces(ids, toy.traces.array)
 
-        assert np.all(trace == simulator.traces)
+        assert np.all(trace == toy.traces.array)
 
-    def test_has_unique_trace(self, simulator, new_component, sniffer):
+    def test_has_unique_trace(self, toy, new_component, sniffer):
         new_fp, new_tr = new_component
-        simulator.add_cell(
-            Position(width=260, height=260), 30, np.array(range(300, 0, -1)), "cell_1"
+        toy.add_cell(
+            Position(width=260, height=260), 30, np.array(range(toy.n_frames, 0, -1)), "cell_1"
         )
-        ids = sniffer._find_overlap_ids(new_fp, simulator.footprints)
-        traces = sniffer._get_overlapped_traces(ids, simulator.traces)
+        ids = sniffer._find_overlap_ids(new_fp, toy.footprints.array)
+        traces = sniffer._get_overlapped_traces(ids, toy.traces.array)
         dupe = sniffer._get_synced_traces(new_tr, traces)
 
         assert len(dupe) == 1
         assert dupe[0][0] == "cell_0"
         assert np.isclose(dupe[0][1], 1.0)
 
-        simulator.drop_cell("cell_1")
+        toy.drop_cell("cell_1")
 
 
 class TestCataloger:
-    @pytest.fixture(scope="module")
-    def energy_shape(self, single_cell_video):
-        return Energy(EnergyParams(gaussian_std=1.0)).process(single_cell_video)
-
     @pytest.fixture(scope="class")
     def new_component(self, single_cell_video, energy_shape):
-        return SliceNMF(SliceNMFParams(cell_radius=60, validity_threshold=0.8)).process(
-            single_cell_video, energy_shape
-        )
-
-    @pytest.fixture(scope="class")
-    def cataloger(self):
-        return Cataloger.from_specification(
-            spec=NodeSpecification(id="test", type="cala.nodes.iter.detect.catalogue.cataloger")
-        )
+        return SliceNMF.from_specification(
+            spec=NodeSpecification(
+                id="test-slice-nmf",
+                type="cala.nodes.iter.detect.slice_nmf.SliceNMF",
+                params={"cell_radius": 60, "validity_threshold": 0.8},
+            )
+        ).process(Residual(array=single_cell_video.array), energy_shape)
 
     def test_init_with(self, cataloger, new_component):
-        fp, tr = cataloger._init_with(*new_component)
+        new_fp, new_tr = (sch(array=arr) for sch, arr in zip([Footprint, Trace], new_component))
+        fp, tr = cataloger._init_with(new_fp=new_fp, new_tr=new_tr)
 
-        assert np.array_equal(fp.values[0], new_component[0].values)
-        assert np.array_equal(tr.values[0], new_component[1].values)
+        assert np.array_equal(fp.array.values[0], new_fp.array.values)
+        assert np.array_equal(tr.array.values[0], new_tr.array.values)
 
-    def test_register(self, cataloger, new_component, simulator):
+    def test_register(self, cataloger, new_component, toy):
         new_fp, new_tr = new_component
         fp, tr = cataloger._register(
             new_fp=Footprint(array=new_fp),
             new_tr=Trace(array=new_tr),
-            existing_fp=Footprints(array=simulator.footprints),
-            existing_tr=Traces(array=simulator.traces),
+            existing_fp=toy.footprints,
+            existing_tr=toy.traces,
         )
 
         assert np.array_equal(fp.array.values[-1], new_component[0].values)
         assert np.array_equal(tr.array.values[-1], new_component[1].values)
 
-    def test_merge(self, cataloger, simulator, single_cell_video, energy_shape):
-        new_component = SliceNMF(SliceNMFParams(cell_radius=10, validity_threshold=0.8)).process(
-            single_cell_video, energy_shape
-        )
+    def test_merge(self, cataloger, toy, single_cell_video, energy_shape):
+        new_component = SliceNMF.from_specification(
+            spec=NodeSpecification(
+                id="test-slice-nmf",
+                type="cala.nodes.iter.detect.slice_nmf.SliceNMF",
+                params={"cell_radius": 10, "validity_threshold": 0.8},
+            )
+        ).process(Residual(array=single_cell_video.array), energy_shape)
+
         new_fp, new_tr = new_component
         fp, tr = cataloger._merge(
             Footprint(array=new_fp),
             Trace(array=new_tr),
-            Footprints(array=simulator.footprints),
-            Traces(array=simulator.traces),
+            toy.footprints,
+            toy.traces,
             duplicates=[("cell_0", 1.0)],
         )
 
         movie_recon = fp.array @ tr.array
         movie_new_comp = new_fp @ new_tr
-        movie_expected = single_cell_video + movie_new_comp
+        movie_expected = single_cell_video.array + movie_new_comp
 
         np.testing.assert_allclose(movie_recon, movie_expected.transpose(*movie_recon.dims))
