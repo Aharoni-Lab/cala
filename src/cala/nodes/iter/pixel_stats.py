7b1de65765208e94afacc489671e887da4ba9421
Raymond <raymond.w.jang@gmail.com>
2025-07-25 18:32:49 -0700

    feat: fit pixstats for noob. revamp tests

--- src/cala/nodes/iter/pixel_stats.py
+++ src/cala/nodes/iter/pixel_stats.py
@@ -1,13 +1,28 @@
 import xarray as xr
+from noob.node import Node
 
-from cala.models import AXIS, Frame, Movie, Traces
+from cala.models import AXIS, Frame, Movie, Traces, PixStat, PopSnap
 
 
-class PixelStater:
-    pixel_stats_: xr.DataArray = None
+class PixelStater(Node):
+    pixel_stats_: PixStat = None
     """Updated pixel-component sufficient statistics W."""
 
-    def initialize(self, traces: Traces, movie: Movie) -> xr.DataArray:
+    def process(
+        self,
+        traces: Traces | PopSnap = None,
+        frames: Movie = None,
+        new_traces: Traces = None,
+        frame: Frame = None,
+    ) -> PixStat:
+        if frames is None:
+            return self.ingest_frame(frame=frame, traces=traces)
+        elif new_traces is None:
+            return self.initialize(frames=frames, traces=traces)
+        else:
+            return self.ingest_components(frames=frames, new_traces=new_traces)
+
+    def initialize(self, traces: Traces, frames: Movie) -> PixStat:
         """
         This transformer calculates the correlation between each pixel's temporal trace
         and each component's temporal activity. The computation provides a measure of
@@ -29,22 +44,22 @@ class PixelStater:
         Args:
             traces (Traces): Temporal traces of all detected fluorescent components.
                 Shape: (components × time)
-            movie (Movie): Stack of frames up to current timestep.
+            frames (Movie): Stack of frames up to current timestep.
                 Shape: (frames × height × width)
         """
-        t_prime = movie.sizes[AXIS.frames_dim]
+        Y = frames.array
 
-        Y = movie
+        t_prime = Y[AXIS.frame_coord].max().item() + 1
 
-        C = traces  # components x time
+        C = traces.array  # components x time
 
         # Compute W = Y[:, 1:t']C^T/t'
         W = Y @ C.T / t_prime
 
-        self.pixel_stats_ = W
+        self.pixel_stats_ = PixStat(array=W)
         return self.pixel_stats_
 
-    def ingest_frame(self, frame: Frame, traces: Traces) -> xr.DataArray:
+    def ingest_frame(self, frame: Frame, traces: PopSnap) -> PixStat:
         """
         Update pixel statistics using current frame and component.
 
@@ -63,7 +78,7 @@ class PixelStater:
         Args:
             frame (Frame): Current frame y_t.
                 Shape: (height × width)
-            traces (Traces): Current temporal component c_t.
+            traces (PopSnap): Current temporal component c_t.
                 Shape: (components)
         """
         # Compute scaling factors
@@ -72,18 +87,20 @@ class PixelStater:
         new_scale = 1 / (frame_idx + 1)
 
         y_t = frame.array
-        W = self.pixel_stats_
+        W = self.pixel_stats_.array
         c_t = traces.array  # New frame traces
 
         # Update pixel-component statistics W_t
         # W_t = ((t-1)/t)W_{t-1} + (1/t)y_t c_t^T
         W_update = prev_scale * W + new_scale * y_t @ c_t
 
-        self.pixel_stats_ = W_update
+        self.pixel_stats_.array = W_update.reset_coords(
+            [AXIS.timestamp_coord, AXIS.frame_coord], drop=True
+        )
 
         return self.pixel_stats_
 
-    def ingest_component(self, movie: Movie, new_traces: Traces = None) -> xr.DataArray:
+    def ingest_component(self, frames: Movie, new_traces: Traces) -> PixStat:
         """Update pixel statistics with new components.
 
         Updates W_t according to the equation:
@@ -94,22 +111,23 @@ class PixelStater:
             t is the current frame index.
 
         Args:
-            movie (Movie): Stack of frames up to current timestep.
+            frames (Movie): Stack of frames up to current timestep.
             new_traces (Traces): Newly detected components' traces
 
         Returns:
             PixelStater: Updated pixel statistics matrix
         """
-        if new_traces is None:
-            return self.pixel_stats_
-
         # Compute scaling factor (1/t)
-        frame_idx = new_traces[AXIS.frame_coord][-1].item()
+        frame_idx = new_traces.array[AXIS.frame_coord].max().item()
         scale = 1 / (frame_idx + 1)
 
         # Compute outer product of frame and new traces
         # (1/t)Y_buf c_new^T
-        new_stats = scale * (movie @ new_traces)
+        new_stats = scale * (frames.array @ new_traces.array)
 
         # Concatenate with existing pixel stats along component axis
-        return xr.concat([self.pixel_stats_, new_stats], dim=AXIS.component_dim)
+        self.pixel_stats_.array = xr.concat(
+            [self.pixel_stats_.array, new_stats], dim=AXIS.component_dim
+        )
+
+        return self.pixel_stats_
--- tests/test_iter/test_pixel_stats.py
+++ tests/test_iter/test_pixel_stats.py
@@ -1,23 +1,91 @@
 import numpy as np
 import pytest
-import xarray as xr
+from noob.node import NodeSpecification
 
+from cala.models import AXIS, Frame, Traces, Movie, PopSnap
 from cala.nodes.iter.pixel_stats import PixelStater
+from cala.testing.toy import FrameDims, Position, Toy
 
 
-def test_init() -> None:
-    """Test the correctness of the pixel statistics computation."""
-
-    # Run computation
-    initializer.learn_one(traces, video)
-    result = initializer.transform_one().transpose("component", "width", "height")
-
-    label = (video @ traces).transpose("component", "width", "height") / video.sizes["frame"]
-
-    assert np.array_equal(result, label)
+@pytest.fixture(scope="function")
+def pix_stats() -> PixelStater:
+    return PixelStater.from_specification(
+        spec=NodeSpecification(id="pix-stat-test", type="cala.nodes.iter.pixel_stats.PixelStater")
+    )
 
 
-def test_ingest_frame() -> None: ...
+@pytest.fixture
+def separate_cells() -> Toy:
+    n_frames = 50
+
+    return Toy(
+        n_frames=n_frames,
+        frame_dims=FrameDims(width=50, height=50),
+        cell_radii=3,
+        cell_positions=[
+            Position(width=15, height=15),
+            Position(width=15, height=35),
+            Position(width=25, height=25),
+            Position(width=35, height=35),
+        ],
+        cell_traces=[
+            np.zeros(n_frames, dtype=float),
+            np.ones(n_frames, dtype=float),
+            np.array(range(n_frames), dtype=float),
+            np.array(range(n_frames - 1, -1, -1), dtype=float),
+        ],
+    )
 
 
-def test_ingest_component() -> None: ...
+def test_init(pix_stats, separate_cells) -> None:
+    result = pix_stats.initialize(traces=separate_cells.traces, frames=separate_cells.make_movie())
+
+    movie = separate_cells.make_movie()
+
+    for id_, trace in zip(
+        separate_cells.cell_ids, separate_cells.traces.array.transpose(AXIS.component_dim, ...)
+    ):
+        assert np.all(
+            result.array.set_xindex(AXIS.id_coord).sel({AXIS.id_coord: id_})
+            == (trace @ movie.array) / separate_cells.n_frames
+        )
+
+
+def test_ingest_frame(pix_stats, separate_cells) -> None:
+
+    pix_stats.initialize(
+        traces=Traces(array=separate_cells.traces.array.isel({AXIS.frames_dim: slice(None, -1)})),
+        frames=Movie(
+            array=separate_cells.make_movie().array.isel({AXIS.frames_dim: slice(None, -1)})
+        ),
+    )
+
+    result = pix_stats.ingest_frame(
+        frame=Frame(array=separate_cells.make_movie().array.isel({AXIS.frames_dim: -1})),
+        traces=PopSnap(array=separate_cells.traces.array.isel({AXIS.frames_dim: -1})),
+    )
+
+    expected = pix_stats.initialize(
+        traces=separate_cells.traces, frames=separate_cells.make_movie()
+    )
+    assert expected == result
+
+
+def test_ingest_component(pix_stats, separate_cells):
+    pix_stats.initialize(
+        traces=Traces(
+            array=separate_cells.traces.array.isel({AXIS.component_dim: slice(None, -1)})
+        ),
+        frames=separate_cells.make_movie(),
+    )
+
+    result = pix_stats.ingest_component(
+        frames=separate_cells.make_movie(),
+        new_traces=Traces(array=separate_cells.traces.array.isel({AXIS.component_dim: [-1]})),
+    )
+
+    expected = pix_stats.initialize(
+        traces=separate_cells.traces, frames=separate_cells.make_movie()
+    )
+
+    assert expected == result
