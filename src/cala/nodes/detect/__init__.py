fe66483c699beab230212fffdec1daea4afeb567
Raymond <raymond.w.jang@gmail.com>
2025-08-13 20:07:32 -0700

    feat: merge sniffer into catalog

--- src/cala/nodes/detect/__init__.py
+++ src/cala/nodes/detect/__init__.py
@@ -1,6 +1,5 @@
 from .catalog import Cataloger
-from .dupe_sniff import Sniffer
 from .energy import Energy
 from .slice_nmf import SliceNMF
 
-__all__ = [Energy, SliceNMF, Sniffer, Cataloger]
+__all__ = [Energy, SliceNMF, Cataloger]
