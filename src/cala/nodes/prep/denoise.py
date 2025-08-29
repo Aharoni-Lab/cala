3f911395a016c33bc0e9c0fc7580944bfdf3a141
Raymond <raymond.w.jang@gmail.com>
2025-08-29 16:09:20 -0700

    feat: auto denoising calibration with first frame

--- src/cala/nodes/prep/denoise.py
+++ src/cala/nodes/prep/denoise.py
@@ -1,24 +1,55 @@
 from collections.abc import Callable
+from functools import partial
 from typing import Annotated as A
 from typing import Any, Literal
 
 import cv2
 import numpy as np
 import xarray as xr
-from noob import Name
-
+from noob import Name, process_method
+from pydantic import BaseModel
+from skimage.restoration import calibrate_denoiser
 from cala.assets import Frame
 
 
+def _bilateral(arr, **kwargs) -> np.ndarray:
+    arr = arr.astype(np.float32)
+    return cv2.bilateralFilter(arr, **kwargs)
+
+
+class Restore(BaseModel):
+    kwargs: dict[str, Any] | None = None
+    model: Callable = None
+
+    @process_method
+    def denoise(self, frame: Frame) -> A[Frame, Name("frame")]:
+        arr = frame.array
+        if self.model is None:
+            if not self.kwargs:
+                param_matrix = {
+                    "d": list(range(1, 20)),
+                    "sigmaColor": [10, 50, 100, 200, 250],
+                    "sigmaSpace": [10, 50, 100, 200, 250],
+                }
+                self.model = calibrate_denoiser(arr, _bilateral, param_matrix)
+            else:
+                self.model = partial(cv2.bilateralFilter, **self.kwargs)
+
+        denoised = self.model(arr)
+        return Frame.from_array(xr.DataArray(denoised, dims=arr.dims, coords=arr.coords))
+
+
 def denoise(
-    frame: Frame, method: Literal["gaussian", "median", "bilateral"], kwargs: dict[str, Any]
+    frame: Frame,
+    method: Literal["gaussian", "median", "bilateral", "nonlocal"],
+    kwargs: dict[str, Any],
 ) -> A[Frame, Name("frame")]:
     """Denoise a single frame."""
     methods: dict[str, Callable] = {
         "gaussian": cv2.GaussianBlur,
         "median": cv2.medianBlur,
         "bilateral": cv2.bilateralFilter,
-        "nonlocal": cv2.fastNlMeansDenoising,
+        "nonlocal": cv2.fastNlMeansDenoising,  # really slow. ~40 ms.
     }
 
     _func = methods[method]
