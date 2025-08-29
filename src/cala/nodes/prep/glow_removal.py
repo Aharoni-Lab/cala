cafefbf036e92fe9be2853abc32a6017c6a5f9cc
Raymond <raymond.w.jang@gmail.com>
2025-08-29 16:08:32 -0700

    feat: remove getinfo from glow

--- src/cala/nodes/prep/glow_removal.py
+++ src/cala/nodes/prep/glow_removal.py
@@ -20,19 +20,6 @@ class GlowRemover:
         self.base_brightness_ = np.minimum(frame.values, self.base_brightness_)
         self._learn_count += 1
 
-        return Frame.from_array(
-            xr.DataArray(frame - self.base_brightness_, dims=frame.dims, coords=frame.coords)
-        )
+        shifted = (frame - self.base_brightness_).values
 
-    def get_info(self) -> dict:
-        """Get information about the current state.
-
-        Returns
-        -------
-        dict
-            Dictionary containing current statistics
-        """
-        return {
-            "base_brightness_": self.base_brightness_,
-            "learn_count": self._learn_count,
-        }
+        return Frame.from_array(xr.DataArray(shifted, dims=frame.dims, coords=frame.coords))
