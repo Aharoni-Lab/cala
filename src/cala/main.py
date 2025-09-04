864defebf8358e69288fd839c163ffceccaad2e9
Raymond <raymond.w.jang@gmail.com>
2025-09-04 09:21:36 -0700

    chore: cleanup cli entrypoint

--- src/cala/main.py
+++ src/cala/main.py
@@ -1,5 +1,8 @@
+from typing import Annotated
+
 import typer
 import uvicorn
+from noob import SynchronousRunner, Tube
 
 from cala.gui.app import get_app
 
@@ -8,6 +11,10 @@ app = get_app()
 
 
 @cli.command()
-def main(spec: str, gui: bool) -> None:  # put options as main params (look at Typer docs)
+def main(spec: str, gui: Annotated[bool, typer.Option()] = False) -> None:
     if gui:
         uvicorn.run("cala.main:app", reload=True)
+    else:
+        tube = Tube.from_specification(spec)
+        runner = SynchronousRunner(tube=tube)
+        runner.run()
