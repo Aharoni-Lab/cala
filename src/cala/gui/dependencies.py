05c4bf2d0d3857c20277381366674212cf4fd68c
Raymond <raymond.w.jang@gmail.com>
2025-09-04 09:01:08 -0700

    chore: cleanup dependencies

--- src/cala/gui/dependencies.py
+++ src/cala/gui/dependencies.py
@@ -1,17 +1,8 @@
-import os
 from pathlib import Path
 
-from cala.config import Config
-from cala.gui.socket_manager import SocketManager
 
-socket_manager = SocketManager()
-root_path = Path(__file__).parents[3]
-config = Config.from_yaml(root_path / os.getenv("CALA_CONFIG_PATH"))
+def get_frontend_dir():
+    return Path("frontend")
 
 
-async def get_config() -> Config:
-    return config
-
-
-def get_socket_manager() -> SocketManager:
-    return socket_manager
+async def get_config(): ...
