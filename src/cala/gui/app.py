44d42f4720b88146bf33ddef3c296f9b3cfa2a5b
Raymond <raymond.w.jang@gmail.com>
2025-09-04 09:50:02 -0700

    feat: routes initial architecture

--- src/cala/gui/app.py
+++ src/cala/gui/app.py
@@ -2,6 +2,10 @@ from contextlib import asynccontextmanager
 from typing import AsyncGenerator, Any
 
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+
+from cala.gui.dependencies import get_frontend_dir
+from cala.gui.routes import router
 
 
 @asynccontextmanager
@@ -10,10 +14,6 @@ async def lifespan(app: FastAPI) -> AsyncGenerator[None, Any]:
 
 
 def get_app() -> FastAPI:
-    from fastapi.staticfiles import StaticFiles
-    from cala.gui.dependencies import get_frontend_dir
-    from cala.gui.routes import router
-
     app = FastAPI(lifespan=lifespan, debug=True)
 
     app.mount(path="/dist", app=StaticFiles(directory=get_frontend_dir() / "dist"), name="dist")
--- src/cala/gui/routes.py
+++ src/cala/gui/routes.py
@@ -1,54 +1,82 @@
-import importlib
-import os
 from pathlib import Path
+from queue import Queue
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, WebSocket, WebSocketDisconnect
+from fastapi import APIRouter, Depends, WebSocket, BackgroundTasks
 from fastapi.requests import Request
 from fastapi.responses import FileResponse, HTMLResponse
 from fastapi.templating import Jinja2Templates
 from noob import SynchronousRunner
 
-from cala.gui.dependencies import get_frontend_dir
+from cala.gui.dependencies import get_frontend_dir, Spec
+from cala.models.gui import GUISpec
 
 templates = Jinja2Templates(directory=get_frontend_dir() / "templates")
 
 router = APIRouter()
 
+_thread = None
+
+
+class QManager:
+    _qs: dict[str, Queue] = {}
+
+    @classmethod
+    def get_queue(cls, name: str) -> Queue:
+        if cls._qs.get(name) is None:
+            cls._qs[name] = Queue()
+        return cls._qs.get(name)
+
 
 @router.get("/", response_class=HTMLResponse)
-async def get(request: Request, config: Annotated[Config, Depends(get_config)]) -> HTMLResponse:
+async def get(request: Request, spec: Annotated[GUISpec, Depends(Spec)]) -> HTMLResponse:
     """Serve the dashboard page"""
-    return templates.TemplateResponse(request, "index.html", {"config": config.gui.model_dump()})
+    return templates.TemplateResponse(request, "index.html", {"config": spec.model_dump()})
 
 
 @router.websocket("/ws")
-async def websocket_endpoint(
-    websocket: WebSocket, socket_manager: Annotated[SocketManager, Depends(get_socket_manager)]
-) -> None:
+async def websocket_endpoint(websocket: WebSocket) -> None:
     """Handle WebSocket connection and run streamers"""
-    await socket_manager.connect(websocket)
-    try:
-        # some kind of waiting mechanism here to keep the connection open until the client
-        # disconnects
-        while True:
-            await websocket.receive_text()
-    except WebSocketDisconnect:
-        print(
-            f"websocket disconnected, still have {len(socket_manager.active_connections)} active "
-            f"connections"
-        )
-        if websocket in socket_manager.active_connections:
-            socket_manager.disconnect(websocket)
+    q = QManager.get_queue("lineplot")
+
+    await websocket.accept()
+    # some kind of waiting mechanism here
+    # to keep the connection open until the client disconnects
+    while True:
+        event = q.get()
+        if event is None:
+            break
+        await websocket.send_json(event)
 
 
 @router.get("/{node_id}/{filename}")
-async def stream(
-    node_id: str, filename: str, config: Annotated[Config, Depends(get_config)]
-) -> FileResponse:
+async def stream(node_id: str, filename: str) -> FileResponse:
     """Serve HLS stream files"""
-    output_dir = config.output_dir
-    stream_path = output_dir / node_id / filename
+    stream_path = Path("output_dir") / node_id / filename
     if stream_path.exists():
         return FileResponse(str(stream_path))
     raise FileNotFoundError({"AppStreamError": f"Playlist not found: {stream_path}"})
+
+
+@router.post("/start")
+def start(background: BackgroundTasks):
+    global _thread
+    tube = Tube.from_specification()
+    runner = SynchronousRunner(tube=tube)
+
+    def _cb(event):
+        q = QManager.get_queue("lineplot")
+        if event.condition == "what i want":
+            q.put(event)
+
+    runner.add_callback(_cb)
+    background.add_task(runner.run)
+
+
+@router.post("/stop")
+def stop():
+    global _thread
+    q = QManager.get_queue("lineplot")
+    q.put(None)
+    _thread.terminate()
+    _thread.join()
