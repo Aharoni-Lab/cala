f5c83affd00ffb3eb923019dea19a4d7e84ac71b
Raymond <raymond.w.jang@gmail.com>
2025-09-05 14:23:16 -0700

    chore: move gui assets to gui dir
    Signed-off-by: Raymond <raymond.w.jang@gmail.com>

--- src/cala/gui/const.py
+++ src/cala/gui/const.py
@@ -1,5 +1,5 @@
 from pathlib import Path
 
 SPEC_FILE = Path(__file__).parent / "spec.yaml"
-TEMPLATES_DIR = Path(__file__).parents[1] / "templates"
-STATIC_DIR = Path(__file__).parents[1] / "static"
+TEMPLATES_DIR = Path(__file__).parent / "templates"
+STATIC_DIR = Path(__file__).parent / "static"
