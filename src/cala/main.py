57ca0b75fdc4e1030963746a5d4548eab2275a4c
Raymond <raymond.w.jang@gmail.com>
2025-09-05 13:57:07 -0700

    feat: change fastapi reload directory, since we move to frontend dir when we run pdm run dev from root (read: start.working_dir = "frontend" in pyproject.toml)
    Signed-off-by: Raymond <raymond.w.jang@gmail.com>

--- src/cala/main.py
+++ src/cala/main.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from typing import Annotated
 
 import typer
@@ -20,7 +21,7 @@ except TypeError as e:
 @cli.command()
 def main(spec: str, gui: Annotated[bool, typer.Option()] = False) -> None:
     if gui:
-        uvicorn.run("cala.main:app", reload=True)
+        uvicorn.run("cala.main:app", reload=True, reload_dirs=[Path(__file__).parent])
     else:
         tube = Tube.from_specification(spec)
         runner = SynchronousRunner(tube=tube)
